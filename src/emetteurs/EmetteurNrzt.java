e1cce4e9a90d9da81aba3d080a235ccc1aae2469
ascherre <ascherre@9958f51f-d44f-464b-adcd-29ca957f21a5>
2015-10-01 09:11:22 +0000

    Nrzt OK

--- src/emetteurs/EmetteurNrzt.java
+++ src/emetteurs/EmetteurNrzt.java
@@ -1,6 +1,71 @@
 package emetteurs;
 
-import
-public class EmetteurNrzt {
+
+import transmetteurs.Transmetteur;
+import destinations.DestinationInterface;
+import information.*;
+
+public class EmetteurNrzt extends Transmetteur<Boolean,Float>{
+	private int nbEch ;
+	private float ampMin;
+	private float ampMax;
+	
+	public EmetteurNrzt(){
+		super();
+		nbEch = 30;
+		ampMin = 0.0f;
+		ampMax = 1.0f;
+	}
+	public void setnbEch(int nbEch){
+		this.nbEch = nbEch;
+	}
+	public void setampMin(float ampMin){
+		this.ampMin = ampMin;
+	}
+	public void setampMax(float ampMax){
+		this.ampMax = ampMax;
+	}
+	
+    public void recevoir(Information <Boolean> information) throws InformationNonConforme {
+ 	   this.informationRecue = information;
+  	  if (informationRecue == null)
+ 			throw new InformationNonConforme("Erreur : Information non conforme");
+   	  else
+   		informationRecue = information;
+  	  	emettre();
+    }
+    
+	 /**
+	 * �met l'information construite par le transmetteur  
+	 */
+      public void emettre() throws InformationNonConforme{
+ 	  if (informationRecue == null)
+			throw new InformationNonConforme("Erreur : Information non conforme");
+ 	  else{
+
+		 for (int i=0; i<informationRecue.nbElements();i++) {
+			 if(informationRecue.iemeElement(i) == true){
+				 for(int j=0; j<nbEch/3; j++){
+					 informationEmise.add(ampMax*(1/3)*nbEch);
+				 }
+				 for(int j =nbEch; j<nbEch*2/3; j++){
+					 informationEmise.add(ampMax);
+				 }
+				 for(int j =nbEch*2/3; j<nbEch; j++){
+					 informationEmise.add(-ampMax*(1/3)*nbEch);
+				 }
+			 }
+			 else{
+				 for(int j =0; j<nbEch; j++){
+					 informationEmise.add(ampMin);
+				 }
+			 }
+ 		  }
+ 		  		  
+ 		  for (DestinationInterface <Float> destinationConnectee : destinationsConnectees) {
+ 			  destinationConnectee.recevoir(informationEmise);
+ 		  }
+ 	  }
+   }
 
 }
