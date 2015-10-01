28e64876951fab18a4861fcda75cc9f9e4a34aed
sdanie01 <sdanie01@9958f51f-d44f-464b-adcd-29ca957f21a5>
2015-10-01 08:22:01 +0000

    Commit du recepteur NRZ

--- src/recepteurs/RecepteurNrz.java
+++ src/recepteurs/RecepteurNrz.java
@@ -1,5 +1,40 @@
 package recepteurs;
 
-public class RecepteurNrz {
+import destinations.DestinationInterface;
+import information.Information;
+import information.InformationNonConforme;
+import transmetteurs.Transmetteur;
 
+public class RecepteurNrz extends Transmetteur<Float,Boolean> 
+{
+
+	@Override
+	public void recevoir(Information<Float> information) throws InformationNonConforme 
+	{
+		this.informationRecue = information;
+	  	  if (informationRecue == null)
+	 			throw new InformationNonConforme("Erreur : Information non conforme");
+	   	  else
+	   		informationRecue = information;
+	  	  	emettre();
+	}
+
+	@Override
+	public void emettre() throws InformationNonConforme 
+	{
+		 if (informationRecue == null)
+				throw new InformationNonConforme("Erreur : Information non conforme");
+	 	  else
+	 	  {
+
+	 		  this.informationEmise = informationRecue;
+
+			 for (DestinationInterface <Float> destinationConnectee : destinationsConnectees) 
+			 {
+	 			  destinationConnectee.recevoir(informationEmise);
+		
+			 }
+
+	 	  }
+	}
 }
