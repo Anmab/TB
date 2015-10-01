727dc9224d2c5167d38f6bcaa53d6972d16fec04
ascherre <ascherre@9958f51f-d44f-464b-adcd-29ca957f21a5>
2015-10-01 09:54:42 +0000

    NRZT changement des équations des pentes

--- src/emetteurs/EmetteurNrzt.java
+++ src/emetteurs/EmetteurNrzt.java
@@ -37,13 +37,13 @@ public class EmetteurNrzt extends Transmetteur<Boolean,Float>{
 		 for (int i=0; i<informationRecue.nbElements();i++) {
 			 if(informationRecue.iemeElement(i) == true){
 				 for(int j=0; j<nbEch/3; j++){
-					 informationEmise.add(ampMax/((1/3)*nbEch));
+					 informationEmise.add((ampMax-ampMin)/((1/3)*nbEch));
 				 }
 				 for(int j =nbEch/3; j<nbEch*2/3; j++){
 					 informationEmise.add(ampMax);
 				 }
 				 for(int j =nbEch*2/3; j<nbEch; j++){
-					 informationEmise.add(-ampMax/((1/3)*nbEch));
+					 informationEmise.add((ampMin-ampMax)/(nbEch-(2/3)*nbEch));
 				 }
 			 }
 			 else{
