70c42b22a2f28a9d59513c79a5e21e28e7989121
sdanie01 <sdanie01@9958f51f-d44f-464b-adcd-29ca957f21a5>
2015-10-01 09:51:44 +0000

    RecepteurNRZT

--- src/recepteurs/RecepteurNrzt.java
+++ src/recepteurs/RecepteurNrzt.java
@@ -1,5 +1,57 @@
 package recepteurs;
 
-public class RecepteurNrzt {
+import information.Information;
+import information.InformationNonConforme;
+import transmetteurs.Transmetteur;
+import destinations.DestinationInterface;
 
+public class RecepteurNrzt extends Transmetteur<Float,Boolean> 
+{
+	private int nbEch ;
+	private float ampMin;
+	private float ampMax;
+
+	public RecepteurNrzt(int nbEch, float ampMin, float ampMax)	
+	{
+		super();
+		this.nbEch = nbEch;
+		this.ampMin = ampMin;
+		this.ampMax = ampMax;
+	}
+
+	
+	@Override
+	public void recevoir(Information<Float> information) throws InformationNonConforme 
+	{
+		this.informationRecue = information;
+		if (informationRecue == null)
+			throw new InformationNonConforme("Erreur : Information non conforme");
+		else
+			informationRecue = information;
+		emettre();
+
+	}
+	@Override
+	public void emettre() throws InformationNonConforme {
+		  if (informationRecue == null)
+				throw new InformationNonConforme("Erreur : Information non conforme");
+		  
+	 	  else 
+	 	  {
+	 		 for (int i=0; i<informationRecue.nbElements();i++)
+	 			 { if (nbEch/2 == ampMax)
+	 				 informationEmise.add(true);
+	 			 else
+	 				 informationEmise.add(false);
+	 			 }
+				
+	 		  		  
+	 		  for (DestinationInterface<Boolean> destinationConnectee : destinationsConnectees) 
+	 		  {
+	 			  destinationConnectee.recevoir(informationEmise);
+	 		  }
+	 	  }
+	}
 }
+	
+
