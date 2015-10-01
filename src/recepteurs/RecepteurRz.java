0c6c528acd909abb865a01484930ee17c9369d1c
adunan <adunan@9958f51f-d44f-464b-adcd-29ca957f21a5>
2015-10-01 11:51:05 +0000

    Corrigé

--- src/recepteurs/RecepteurRz.java
+++ src/recepteurs/RecepteurRz.java
@@ -1,40 +1,58 @@
 package recepteurs;
-import destinations.DestinationInterface;
+
 import information.Information;
 import information.InformationNonConforme;
-import transmetteurs.*;
+import transmetteurs.Transmetteur;
+import destinations.DestinationInterface;
 
-public class RecepteurRz extends Transmetteur<Boolean,Float> {
-	public RecepteurRz(){
+public class RecepteurRz extends Transmetteur<Float,Boolean> 
+{
+	private int nbEch ;
+	private float ampMin;
+	private float ampMax;
+
+	public RecepteurRz(int nbEch, float ampMin, float ampMax)	
+	{
 		super();
+		this.nbEch = nbEch;
+		this.ampMin = ampMin;
+		this.ampMax = ampMax;
 	}
+
 	
-    public void recevoir(Information <Boolean> information) throws InformationNonConforme {
- 	   this.informationRecue = information;
-  	  if (informationRecue == null)
- 			throw new InformationNonConforme("Erreur : Information non conforme");
-   	  else
-   		informationRecue = information;
-  	  	emettre();
-    }
- /**
- * �met l'information construite par le transmetteur  
- */
-   public void emettre() throws InformationNonConforme{
- 	  if (informationRecue == null)
+	@Override
+	public void recevoir(Information<Float> information) throws InformationNonConforme 
+	{
+		this.informationRecue = information;
+		if (informationRecue == null)
 			throw new InformationNonConforme("Erreur : Information non conforme");
- 	  else{
- 		  // Transmision parfaite
- 		  this.informationEmise = informationRecue;
- 		  //Transmition erroné
- 		  /*
- 		  Information informationEmise = new Information() ;
- 		  for (int i = 0 ; i < informationRecue.nbElements(); i++)
- 			  informationEmise.add(true);
- 			  */
- 		  for (DestinationInterface <Float> destinationConnectee : destinationsConnectees) {
+		else
+			informationRecue = information;
+		emettre();
+
+	}
+	@Override
+	public void emettre() throws InformationNonConforme {
+		  if (informationRecue == null)
+				throw new InformationNonConforme("Erreur : Information non conforme");
+		  
+	 	  else {
+	 		 for (int i=nbEch/2; i<informationRecue.nbElements();i+=nbEch){
+	 			 if (informationRecue.iemeElement(i) >= (ampMax-ampMin / 2)){
+	 				informationEmise.add(true);
+	 			 }
+	 			 else{
+	 				informationEmise.add(false);
+	 			 }
+	 				 
+	 		 }
+	 	  }
+		  
+	 		  		  
+ 		  for (DestinationInterface<Boolean> destinationConnectee : destinationsConnectees) 
+ 		  {
  			  destinationConnectee.recevoir(informationEmise);
  		  }
- 	  }
-	  }
+	}
 }
+	
