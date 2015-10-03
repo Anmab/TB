33d37ef1e75e2abf872c7aaf2a60e81716a53baa
adunan <adunan@9958f51f-d44f-464b-adcd-29ca957f21a5>
2015-10-03 19:15:02 +0000

    Test Tp2 Sans TP1 OK

--- src/test/SimulateurTestTP2.java
+++ src/test/SimulateurTestTP2.java
@@ -27,68 +27,196 @@ public class SimulateurTestTP2 {
 		}
 	
 	@Test
-	public void testTP2() throws Exception{
-		System.out.println("Tests du Simulateur :");
-		System.out.println("	Chaîne transmission analogique parfaite en Java séance 2 :");
-				
-
-		String options1[] = {"-form","RZ"};
-		simulateur = new Simulateur(options1);
+	public void testFormSimple() throws Exception{
+		
+		// Test RZ simple
+		String options[] = {"-form","RZ"};
+		simulateur = new Simulateur(options);
 		simulateur.execute();
 		collector.checkThat(simulateur.calculTauxErreurBinaire(),is((float)0));
 		
-		String options2[] = {"-form","NRZ"};
+		// Test NRZ simple
+		options[1]= "NRZ";
+		simulateur = new Simulateur(options);
+		simulateur.execute();
+		collector.checkThat(simulateur.calculTauxErreurBinaire(),is((float)0));
+		
+		// Test NRZT simple
+		options[1]= "NRZT";
+		simulateur = new Simulateur(options);
+		simulateur.execute();
+		collector.checkThat(simulateur.calculTauxErreurBinaire(),is((float)0));
+	}
+	
+	@Test
+	public void testRZ() throws Exception{
+		
+		// Test RZ simple
+		String options[] = {"-form","RZ"};
+		simulateur = new Simulateur(options);
+		simulateur.execute();
+		collector.checkThat(simulateur.calculTauxErreurBinaire(),is((float)0));
+		
+		// Test RZ avec sonde
+		String options2[] = {"-form","RZ","-s"};
 		simulateur = new Simulateur(options2);
 		simulateur.execute();
 		collector.checkThat(simulateur.calculTauxErreurBinaire(),is((float)0));
 		
-		String options3[] = {"-form","NRZT"};
+		// Test RZ avec amplitude positive
+		String options3[] = {"-form","RZ","-ampl","1","2"};
 		simulateur = new Simulateur(options3);
 		simulateur.execute();
 		collector.checkThat(simulateur.calculTauxErreurBinaire(),is((float)0));
-		
-		String options4[] = {"-form","NRZT","-nbEch","1000"};
+
+		// Test RZ avec amplitude negative
+		String options4[] = {"-form","RZ","-ampl","-2","2"};
 		simulateur = new Simulateur(options4);
 		simulateur.execute();
 		collector.checkThat(simulateur.calculTauxErreurBinaire(),is((float)0));
 		
-		String options5[] = {"-form","NRZ","-nbEch","100"};
+		// Test RZ avec amplitude negative
+		String options5[] = {"-form","RZ","-ampl","-2","-1"};
 		simulateur = new Simulateur(options5);
 		simulateur.execute();
 		collector.checkThat(simulateur.calculTauxErreurBinaire(),is((float)0));
 		
-		String options6[] = {"-form","RZ","-nbEch","10"};
+		// Test RZ avec amplitude negative et nulle
+		String options6[] = {"-form","RZ","-ampl","-2","0"};
 		simulateur = new Simulateur(options6);
 		simulateur.execute();
 		collector.checkThat(simulateur.calculTauxErreurBinaire(),is((float)0));
 		
-		String options7[] = {"-form","NRZT","-ampl","1","10"};
+		// Test RZ avec amplitude positive et nulle
+		String options7[] = {"-form","RZ","-ampl","0","10"};
 		simulateur = new Simulateur(options7);
 		simulateur.execute();
 		collector.checkThat(simulateur.calculTauxErreurBinaire(),is((float)0));
 		
-		String options8[] = {"-form","NRZ","-ampl","-2","9"};
+		// Test RZ avec nombre d'echantillons
+		String options8[] = {"-form","RZ","-nbEch","10"};
 		simulateur = new Simulateur(options8);
 		simulateur.execute();
 		collector.checkThat(simulateur.calculTauxErreurBinaire(),is((float)0));
 		
-		String options9[] = {"-form","RZ","-ampl","0","10"};
+		// Test RZ combiné
+		String options9[] = {"-form","RZ","-nbEch","10","-ampl","1","10","-s"};
 		simulateur = new Simulateur(options9);
 		simulateur.execute();
 		collector.checkThat(simulateur.calculTauxErreurBinaire(),is((float)0));
+	}
+	@Test
+	public void testNRZ() throws Exception{
 		
-		String options10[] = {"-form","NRZT","-ampl","1","10","-s"};
-		simulateur = new Simulateur(options10);
+		// Test NRZ simple
+		String options[] = {"-form","NRZ"};
+		simulateur = new Simulateur(options);
 		simulateur.execute();
 		collector.checkThat(simulateur.calculTauxErreurBinaire(),is((float)0));
 		
-		String options11[] = {"-form","NRZ","-ampl","2","9","-s"};
-		simulateur = new Simulateur(options11);
+		// Test NRZ avec sonde
+		String options2[] = {"-form","NRZ","-s"};
+		simulateur = new Simulateur(options2);
 		simulateur.execute();
 		collector.checkThat(simulateur.calculTauxErreurBinaire(),is((float)0));
 		
-		String options12[] = {"-form","RZ","-ampl","0","10","-s"};
-		simulateur = new Simulateur(options12);
+		// Test NRZ avec amplitude positive
+		String options3[] = {"-form","NRZ","-ampl","1","2"};
+		simulateur = new Simulateur(options3);
+		simulateur.execute();
+		collector.checkThat(simulateur.calculTauxErreurBinaire(),is((float)0));
+
+		// Test NRZ avec amplitude negative
+		String options4[] = {"-form","NRZ","-ampl","-2","2"};
+		simulateur = new Simulateur(options4);
+		simulateur.execute();
+		collector.checkThat(simulateur.calculTauxErreurBinaire(),is((float)0));
+		
+		// Test NRZ avec amplitude negative
+		String options5[] = {"-form","NRZ","-ampl","-2","-1"};
+		simulateur = new Simulateur(options5);
+		simulateur.execute();
+		collector.checkThat(simulateur.calculTauxErreurBinaire(),is((float)0));
+		
+		// Test NRZ avec amplitude negative et nulle
+		String options6[] = {"-form","NRZ","-ampl","-2","0"};
+		simulateur = new Simulateur(options6);
+		simulateur.execute();
+		collector.checkThat(simulateur.calculTauxErreurBinaire(),is((float)0));
+		
+		// Test NRZ avec amplitude positive et nulle
+		String options7[] = {"-form","NRZ","-ampl","0","10"};
+		simulateur = new Simulateur(options7);
+		simulateur.execute();
+		collector.checkThat(simulateur.calculTauxErreurBinaire(),is((float)0));
+		
+		// Test NRZ avec nombre d'echantillons
+		String options8[] = {"-form","NRZ","-nbEch","10"};
+		simulateur = new Simulateur(options8);
+		simulateur.execute();
+		collector.checkThat(simulateur.calculTauxErreurBinaire(),is((float)0));
+		
+		// Test NRZ combiné
+		String options9[] = {"-form","NRZ","-nbEch","10","-ampl","1","10","-s"};
+		simulateur = new Simulateur(options9);
+		simulateur.execute();
+		collector.checkThat(simulateur.calculTauxErreurBinaire(),is((float)0));
+	}
+	
+	@Test
+	public void testNRZT() throws Exception{
+		
+		// Test NRZT simple
+		String options[] = {"-form","NRZT"};
+		simulateur = new Simulateur(options);
+		simulateur.execute();
+		collector.checkThat(simulateur.calculTauxErreurBinaire(),is((float)0));
+		
+		// Test NRZT avec sonde
+		String options2[] = {"-form","NRZT","-s"};
+		simulateur = new Simulateur(options2);
+		simulateur.execute();
+		collector.checkThat(simulateur.calculTauxErreurBinaire(),is((float)0));
+		
+		// Test NRZT avec amplitude positive
+		String options3[] = {"-form","NRZT","-ampl","1","2"};
+		simulateur = new Simulateur(options3);
+		simulateur.execute();
+		collector.checkThat(simulateur.calculTauxErreurBinaire(),is((float)0));
+
+		// Test NRZT avec amplitude negative
+		String options4[] = {"-form","NRZT","-ampl","-2","2"};
+		simulateur = new Simulateur(options4);
+		simulateur.execute();
+		collector.checkThat(simulateur.calculTauxErreurBinaire(),is((float)0));
+		
+		// Test NRZT avec amplitude negative
+		String options5[] = {"-form","NRZT","-ampl","-2","-1"};
+		simulateur = new Simulateur(options5);
+		simulateur.execute();
+		collector.checkThat(simulateur.calculTauxErreurBinaire(),is((float)0));
+		
+		// Test NRZT avec amplitude negative et nulle
+		String options6[] = {"-form","NRZT","-ampl","-2","0"};
+		simulateur = new Simulateur(options6);
+		simulateur.execute();
+		collector.checkThat(simulateur.calculTauxErreurBinaire(),is((float)0));
+		
+		// Test NRZT avec amplitude positive et nulle
+		String options7[] = {"-form","NRZT","-ampl","0","10"};
+		simulateur = new Simulateur(options7);
+		simulateur.execute();
+		collector.checkThat(simulateur.calculTauxErreurBinaire(),is((float)0));
+		
+		// Test NRZT avec nombre d'echantillons
+		String options8[] = {"-form","NRZT","-nbEch","10"};
+		simulateur = new Simulateur(options8);
+		simulateur.execute();
+		collector.checkThat(simulateur.calculTauxErreurBinaire(),is((float)0));
+		
+		// Test NRZT combiné
+		String options9[] = {"-form","NRZT","-nbEch","10","-ampl","1","10","-s"};
+		simulateur = new Simulateur(options9);
 		simulateur.execute();
 		collector.checkThat(simulateur.calculTauxErreurBinaire(),is((float)0));
 	}
