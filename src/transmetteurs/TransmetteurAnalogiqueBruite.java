0ae6f147527ff6ac6a8918c9ca50c3ca65169999
ascherre <ascherre@9958f51f-d44f-464b-adcd-29ca957f21a5>
2015-10-06 07:39:49 +0000

    EmetteurAnalogiqueBruite

---
+++ src/transmetteurs/TransmetteurAnalogiqueBruite.java
@@ -0,0 +1,56 @@
+package transmetteurs;
+import destinations.*;
+import information.*;
+import java.math.*;
+import java.util.*;
+
+public class TransmetteurAnalogiqueBruite extends Transmetteur <Float,Float>{
+	private float snr;
+	
+	public TransmetteurAnalogiqueBruite(float snr) {
+		super();
+		this.snr=snr;
+	}
+	
+	   /**
+	    * re�oit une information. 
+		 * Cette m�thode, en fin d'ex�cution, appelle la m�thode �mettre.
+	    * @param information  l'information  re�ue
+	    */
+	       public void recevoir(Information <Float> information) throws InformationNonConforme {
+	    	   this.informationRecue = information;
+	     	  if (informationRecue == null)
+	    			throw new InformationNonConforme("Erreur : Information non conforme");
+	      	  else
+	      		informationRecue = information;
+	     	  	emettre();
+	       }
+	       /**
+	        * �met l'information construite par le transmetteur  
+	        */
+	          public void emettre() throws InformationNonConforme{
+	        	  float a1=0;
+	        	  float a2=0;
+	        	  float tau=0;
+	        	  
+	        	  if (informationRecue == null)
+	      			throw new InformationNonConforme("Erreur : Information non conforme");
+	        	  else{ 
+	        		  informationEmise= new Information(); 
+	        		  
+	        		  for(int i =0 ; i<informationRecue.nbElements() ; i++) {
+	        			  a1= (float)Math.random();
+		        		  a2= (float)Math.random();
+		        		  float b = (float)(tau*Math.sqrt((-2)*Math.log10(1-a1))*Math.cos(2*Math.PI*a2));
+	        			  informationEmise.add((float)informationRecue.iemeElement(i)+b);
+	        		  }
+	        		
+	        		  for (DestinationInterface <Float> destinationConnectee : destinationsConnectees) {
+	        			  destinationConnectee.recevoir(informationEmise);
+	        		  }
+	        	  }
+	    	  }
+	
+	
+	
+}
