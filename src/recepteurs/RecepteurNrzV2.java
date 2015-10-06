0a31478524c7cef69de57a3b8ef62e8b5bf83542
ascherre <ascherre@9958f51f-d44f-464b-adcd-29ca957f21a5>
2015-10-06 09:51:36 +0000

    Recepteurs V2

---
+++ src/recepteurs/RecepteurNrzV2.java
@@ -0,0 +1,73 @@
+package recepteurs;
+
+import destinations.DestinationInterface;
+import information.Information;
+import information.InformationNonConforme;
+import transmetteurs.Transmetteur;
+import java.math.*;
+
+public class RecepteurNrzV2 extends Transmetteur<Float,Boolean> 
+{
+	private int nbEch ;
+	private float ampMin;
+	private float ampMax;
+	/**
+	 *    @param nbEch  nombre d'échantillon
+	 *    @param ampMin  amplitude minimale
+	 *    @param ampMax  amplitude maximale  
+	 */   
+	
+	public RecepteurNrzV2(int nbEch, float ampMin, float ampMax)	
+	{
+		super();
+		this.nbEch = nbEch;
+		this.ampMin = ampMin;
+		this.ampMax = ampMax;
+	}
+	   /**
+	    * reçoit une information. 
+		 * Cette méthode, en fin d'exécution, appelle la méthode émettre.
+	    * @param information  l'information  ré�ue
+	    * @throws InformationNonConforme si un informationRecue est "null"
+	    */
+	@Override
+	public void recevoir(Information<Float> information) throws InformationNonConforme 
+	{
+		this.informationRecue = information;
+	  	  if (informationRecue == null)
+	 			throw new InformationNonConforme("Erreur : Information non conforme");
+	   	  else
+	   		informationRecue = information;
+	  	  	emettre();
+	}
+/**
+ * Transforme une information analogique (Information<Float>) en information logique
+ * Au millieu d'un symbole, on test sa valeur pour déterminé la nature de l'information
+ */
+	@Override
+	public void emettre() throws InformationNonConforme {
+		Information informationEmise = new Information() ;
+		  if (informationRecue == null)
+				throw new InformationNonConforme("Erreur : Information non conforme");
+		  
+	 	  else {
+	 		  
+	 		 for (int i=nbEch/3; i<informationRecue.nbElements();i+=nbEch){
+	 			for (int j=2*nbEch/3; j<informationRecue.nbElements();j+=nbEch){
+	 				if ((informationRecue.iemeElement(i) >= (ampMax+ampMin)/ 2)){
+	 					informationEmise.add(true);
+	 				}
+	 				else{
+	 					informationEmise.add(false);
+	 				}
+	 			}	 
+	 		 }
+	 	  }
+		    		  
+ 		  for (DestinationInterface<Boolean> destinationConnectee : destinationsConnectees) 
+ 		  {
+ 			  destinationConnectee.recevoir(informationEmise);
+ 		  }
+	}
+}
+	
---
+++ src/recepteurs/RecepteurRzV2.java
@@ -0,0 +1,72 @@
+package recepteurs;
+
+import information.Information;
+import information.InformationNonConforme;
+import transmetteurs.Transmetteur;
+import destinations.DestinationInterface;
+
+public class RecepteurRzV2 extends Transmetteur<Float,Boolean> 
+{
+	private int nbEch ;
+	private float ampMin;
+	private float ampMax;
+	/**
+	 *    @param nbEch  nombre d'échantillon
+	 *    @param ampMin  amplitude minimale
+	 *    @param ampMax  amplitude maximale  
+	 */   
+	
+	public RecepteurRzV2(int nbEch, float ampMin, float ampMax)	
+	{
+		super();
+		this.nbEch = nbEch;
+		this.ampMin = ampMin;
+		this.ampMax = ampMax;
+	}
+
+	   /**
+	    * reçoit une information. 
+		 * Cette méthode, en fin d'exécution, appelle la méthode émettre.
+	    * @param information  l'information  ré�ue
+	    * @throws InformationNonConforme si un informationRecue est "null"
+	    */
+	@Override
+	public void recevoir(Information<Float> information) throws InformationNonConforme 
+	{
+		this.informationRecue = information;
+		if (informationRecue == null)
+			throw new InformationNonConforme("Erreur : Information non conforme");
+		else
+			informationRecue = information;
+		emettre();
+
+	}
+	/**
+	 * Transforme une information analogique (Information<Float>) en information logique
+	 * Au millieu d'un symbole, on test sa valeur pour déterminé la nature de l'information
+	 */
+	@Override
+	public void emettre() throws InformationNonConforme {
+		Information informationEmise = new Information() ;
+		  if (informationRecue == null)
+				throw new InformationNonConforme("Erreur : Information non conforme");
+		  
+	 	  else {
+	 		 for (int i=nbEch/2; i<informationRecue.nbElements();i+=nbEch){
+	 			 if (informationRecue.iemeElement(i) >= ((ampMax+ampMin) / 2)){
+	 				informationEmise.add(true);
+	 			 }
+	 			 else{
+	 				informationEmise.add(false);
+	 			 }
+	 				 
+	 		 }
+	 	  }
+		    		  
+ 		  for (DestinationInterface<Boolean> destinationConnectee : destinationsConnectees) 
+ 		  {
+ 			  destinationConnectee.recevoir(informationEmise);
+ 		  }
+	}
+}
+	
