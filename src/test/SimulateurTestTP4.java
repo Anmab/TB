0ce2ed98fe968b2c945fcfae191219eb8ffa52f8
ascherre <ascherre@9958f51f-d44f-464b-adcd-29ca957f21a5>
2015-10-12 12:08:33 +0000

    SimulateurTestTP4

---
+++ src/test/SimulateurTestTP4.java
@@ -0,0 +1,164 @@
+package test;
+
+import static org.hamcrest.CoreMatchers.*;
+import simulateur.ArgumentsException;
+import simulateur.Simulateur;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ErrorCollector;
+
+
+
+public class SimulateurTestTP4 {
+		private Simulateur simulateur = null;		
+	/**
+	*  Permet d'utilise la fonction collector.checkThat(valider(thrown),is(true)); pour tester la levee d'exception
+	* @param arg
+	* @return
+	*/
+
+	@Rule
+	public ErrorCollector collector = new ErrorCollector();
+		
+	@Before
+	public void setUp() throws Exception {
+
+	}
+	//dans les trois fonctions diff�rentes le TEB pour un SNR sera toujours de 0. Nous ne pouvons tester que cette valeur
+	// car nous savons � l'avance le TEB qui correspond � ce SNR.
+	// Les tests suivants vont nous permettre de v�rifier que m�me avec du bruit pour un SNR de 0 on aura un TEB de 0
+	
+	public void TestNRZMultiTrajetBruite() throws Exception{
+			
+		System.out.println("Tests du Simulateur :");
+		System.out.println("	Chaine transmission analogique Multi Trajet et bruitee:");
+		
+				
+		// Avec message predefini puis codage NRZ
+		String options2[] = {"-mess","0101010101010101111111","-form","NRZ", "- snr 0", "-ti 1 3 0.9"};
+		simulateur = new Simulateur(options2);
+		simulateur.execute();
+		collector.checkThat(simulateur.calculTauxErreurBinaire(),is((float)0));
+		
+		// Avec message predefini et sonde  puis codage NRZ
+		String options3[] = {"-mess","0101001010101010101111111","-form","NRZ","-s", "- snr 0", "-ti 1 3 0.9"};
+		simulateur = new Simulateur(options3);
+		simulateur.execute();
+		collector.checkThat(simulateur.calculTauxErreurBinaire(),is((float)0));
+		
+		// Avec message aleatoire  puis codage NRZ
+		String options4[] = {"-mess","48","-form","NRZ" , "- snr 0"};
+		simulateur = new Simulateur(options4);
+		simulateur.execute();
+		collector.checkThat(simulateur.calculTauxErreurBinaire(),is((float)0));
+		
+		// Avec message aleatoire et sonde  puis codage NRZ
+		String options5[] = {"-mess","88","-form","NRZ","-s" , "- snr 0"};
+		simulateur = new Simulateur(options5);
+		simulateur.execute();
+		collector.checkThat(simulateur.calculTauxErreurBinaire(),is((float)0));
+		
+		// Avec message aleatoire et seed  puis codage NRZ
+		String options6[] = {"-mess","42","-form","NRZ","-seed","1" , "- snr 0"};
+		simulateur = new Simulateur(options6);
+		simulateur.execute();
+		collector.checkThat(simulateur.calculTauxErreurBinaire(),is((float)0));
+		
+		// Avec message aleatoire et seed de 100  puis codage NRZ
+		String options7[] = {"-seed","1","-form","NRZ", "- snr 0"};
+		simulateur = new Simulateur(options7);
+		simulateur.execute();
+		collector.checkThat(simulateur.calculTauxErreurBinaire(),is((float)0));
+	}
+
+	public void TestNRZTBruite() throws Exception{
+		
+		System.out.println("Tests du Simulateur :");
+		System.out.println("	Chaine transmission analogique bruitee en Java seance 3 :");
+		
+				
+		// Avec message predefini puis codage NRZT
+		String options2[] = {"-mess","0101010101010101111111","-form","NRZT", "- snr 0"};
+		simulateur = new Simulateur(options2);
+		simulateur.execute();
+		collector.checkThat(simulateur.calculTauxErreurBinaire(),is((float)0));
+		
+		// Avec message predefini et sonde  puis codage NRZT
+		String options3[] = {"-mess","0101001010101010101111111","-form","NRZT","-s", "- snr 0"};
+		simulateur = new Simulateur(options3);
+		simulateur.execute();
+		collector.checkThat(simulateur.calculTauxErreurBinaire(),is((float)0));
+		
+		// Avec message aleatoire  puis codage NRZT
+		String options4[] = {"-mess","48","-form","NRZT", "- snr 0"};
+		simulateur = new Simulateur(options4);
+		simulateur.execute();
+		collector.checkThat(simulateur.calculTauxErreurBinaire(),is((float)0));
+		
+		// Avec message aleatoire et sonde  puis codage NRZ
+		String options5[] = {"-mess","88","-form","NRZT","-s", "- snr 0"};
+		simulateur = new Simulateur(options5);
+		simulateur.execute();
+		collector.checkThat(simulateur.calculTauxErreurBinaire(),is((float)0));
+		
+		// Avec message aleatoire et seed  puis codage NRZT
+		String options6[] = {"-mess","42","-form","NRZT","-seed","1", "- snr 0"};
+		simulateur = new Simulateur(options6);
+		simulateur.execute();
+		collector.checkThat(simulateur.calculTauxErreurBinaire(),is((float)0));
+		
+		// Avec message aleatoire et seed de 100  puis codage NRZT
+		String options7[] = {"-seed","1","-form","NRZT", "- snr 1"};
+		simulateur = new Simulateur(options7);
+		simulateur.execute();
+		collector.checkThat(simulateur.calculTauxErreurBinaire(),is((float)0));
+	}
+	
+	public void TestRZBruite() throws Exception{
+		System.out.println("Tests du Simulateur :");
+		System.out.println("	Chaine transmission analogique bruitee en Java seance 3 :");
+	
+			
+		// Avec message predefini puis codage RZ
+		String options2[] = {"-mess","0101010101010101111111","-form","NRZT", "- snr 0"};
+		simulateur = new Simulateur(options2);
+		simulateur.execute();
+		collector.checkThat(simulateur.calculTauxErreurBinaire(),is((float)0));
+	
+		// Avec message predefini et sonde  puis codage RZ
+		String options3[] = {"-mess","0101001010101010101111111","-form","NRZT","-s", "- snr 0"};
+		simulateur = new Simulateur(options3);
+		simulateur.execute();
+		collector.checkThat(simulateur.calculTauxErreurBinaire(),is((float)0));
+	
+		// Avec message aleatoire  puis codage RZ
+		String options4[] = {"-mess","48","-form","NRZT", "- snr 0"};
+		simulateur = new Simulateur(options4);
+		simulateur.execute();
+		collector.checkThat(simulateur.calculTauxErreurBinaire(),is((float)0));
+	
+		// Avec message aleatoire et sonde  puis codage RZ
+		String options5[] = {"-mess","88","-form","NRZT","-s", "- snr 0"};
+		simulateur = new Simulateur(options5);
+		simulateur.execute();
+		collector.checkThat(simulateur.calculTauxErreurBinaire(),is((float)0));
+	
+		// Avec message aleatoire et seed  puis codage RZ
+		String options6[] = {"-mess","42","-form","NRZT","-seed","1", "- snr 0"};
+		simulateur = new Simulateur(options6);
+		simulateur.execute();
+		collector.checkThat(simulateur.calculTauxErreurBinaire(),is((float)0));
+		
+		// Avec message aleatoire et seed de 100  puis codage RZ
+		String options7[] = {"-seed","1","-form","NRZT", "- snr 0"};
+		simulateur = new Simulateur(options7);
+		simulateur.execute();
+		float a = simulateur.calculTauxErreurBinaire();
+		System.out.println(a);
+		collector.checkThat(simulateur.calculTauxErreurBinaire(),is((float)0));
+	}
+}
+
+
