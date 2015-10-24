27cf74fafd45292be674fcdc095e96ac34acf56a
sdanie01 <sdanie01@9958f51f-d44f-464b-adcd-29ca957f21a5>
2015-10-24 13:35:23 +0000

    TransducteurReception

--- src/transducteur/TransducteurReception.java
+++ src/transducteur/TransducteurReception.java
@@ -20,22 +20,61 @@ public class TransducteurReception extends Transmetteur<Boolean, Boolean> {
 	public void emettre() throws InformationNonConforme {
 		if (informationRecue == null)
 			throw new InformationNonConforme("Erreur : Information non conforme");
-  	  else {
-  		informationEmise = new Information();
-  	  }
-  	  
+		else 
+			informationEmise = new Information<Boolean>();		
+	/*	
+  	boolean a = false;
+  	boolean b = false;
+  	boolean c = false;
+	*/
+  	
   	for (int i = 0; i < informationRecue.nbElements(); i=i+3)
   	{
-  				
-  	  if (informationRecue.iemeElement(i) == false) {
-  		// TODO
-  	  }
-  	  else {
-  		  // TODO
-  		  
-  	  }
+  		boolean a=this.informationRecue.iemeElement(i);
+  		boolean b=this.informationRecue.iemeElement(i+1);
+  		boolean c=this.informationRecue.iemeElement(i+2);
+/*  		
+  		switch (a && b && c)
+  		{
+  		case ((a==false) && (b==false) && (c==false))
+  		this.informationEmise.add(false);
+  		
+  		case ((a==false) && (b==false) && (c==true))
+  		this.informationEmise.add(true);
+  		
+  		case ((a==false) && (b==true) && (c==false))
+  		this.informationEmise.add(false);
+  		
+  		case ((a==false) && (b==true) && (c==true))
+  		this.informationEmise.add(false);
+  		
+  		case ((a==true) && (b==false) && (c==false))
+  		this.informationEmise.add(true);
+  		
+  		case ((a==true) && (b==false) && (c==true))
+  		this.informationEmise.add(true);
+  		*/
+  		
+		if ((a==false) && (b==false) && (c==false))
+		this.informationEmise.add(false);
+		
+		else if ((a==false) && (b==false) && (c==true))
+		this.informationEmise.add(true);
+		
+		else if ((a==false) && (b==true) && (c==false))
+		this.informationEmise.add(false);
+		
+		else if ((a==false) && (b==true) && (c==true))
+		this.informationEmise.add(false);
+		
+		else if ((a==true) && (b==false) && (c==false))
+		this.informationEmise.add(true);
+		
+		else if ((a==true) && (b==false) && (c==true))
+		this.informationEmise.add(true);
   	}
   	
+	
 	for (DestinationInterface<Boolean> destinationConnectee : destinationsConnectees) {
 		destinationConnectee.recevoir(informationEmise);
 	}
